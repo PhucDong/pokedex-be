fa5c9e6afa3a722de047fea6548eb2874df5733a
Phuc <dongvuminhphuc@gmail.com>
2024-04-20 20:54:19 +0700

    Update code 9th time

--- routes/pokemons.api.js
+++ routes/pokemons.api.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
-const csv = require("csvtojson");
 const path = require("path");
 const jsonFilePath = path.join(process.cwd(), "db.json");
 
-const getPokemons = (req, res, next) => {
+// const getPokemons = (req, res, next) => {
+//   try {
+//     let { page, limit, ...filterQuery } = req.query;
+//     page = parseInt(page) || 1;
+//     limit = parseInt(limit) || 10;
+//     let offset = limit * (page - 1);
+
+//     let currentData = JSON.parse(fs.readFileSync(jsonFilePath));
+//     let results = [];
+//     results = currentData.pokemons;
+
+//     results = results.slice(offset, offset + limit);
+
+//     res.status(200).send(results);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+router.get("/", (req, res, next) => {
   try {
     let { page, limit, ...filterQuery } = req.query;
     page = parseInt(page) || 1;
@@ -22,8 +40,6 @@ const getPokemons = (req, res, next) => {
   } catch (error) {
     next(error);
   }
-};
-
-router.get("/", getPokemons);
+});
 
 module.exports = router;

e0b42207760854cbb74487cc2bbdd89c7e70e3f0
Phuc <dongvuminhphuc@gmail.com>
2024-04-20 18:09:55 +0700

    Update code 3rd time

--- routes/pokemons.api.js
+++ routes/pokemons.api.js
@@ -1,29 +1,51 @@
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const path = require("path");
 
 // turn csv file's data into JSON data
 // return that data to the front-end to render it
-const getPokemons = async (req, res, next) => {
+const createPokemonData = async (req, res, next) => {
+  let pokemonImageFiles = fs.readdirSync("./public/images"); // return all files (name & its extension)
+  const pokemonNames = pokemonImageFiles.map((item) => path.parse(item).name); // return only file names (file extensions are not included)
+
+  pokemonImageFiles.forEach((file, index) => {
+    fs.renameSync(
+      `./public/images/${file}`,
+      `./public/images/${pokemonNames[index]}.png`
+    );
+  });
+
+  let newData = await csv().fromFile("pokemon.csv");
+  newData = newData.filter((item) => pokemonNames.includes(item.Name));
+
+  newData = newData.map((pokemon, index) => {
+    return {
+      id: index + 1,
+      url: `/images/${pokemon.Name}.png`,
+      name: pokemon.Name,
+      types:
+        pokemon.Type2 !== ""
+          ? [pokemon.Type1.toLowerCase(), pokemon.Type2.toLowerCase()]
+          : [pokemon.Type1.toLowerCase()],
+    };
+  });
+
+  let currentData = JSON.parse(fs.readFileSync("db.json"));
+  currentData.pokemons = newData;
+  fs.writeFileSync("db.json", JSON.stringify(currentData));
+};
+
+const getPokemons = (req, res, next) => {
   try {
     let { page, limit, ...filterQuery } = req.query;
     page = parseInt(page) || 1;
     limit = parseInt(limit) || 10;
     let offset = limit * (page - 1);
 
-    let newData = await csv().fromFile("pokemon.csv");
-    newData = newData.map((newPokemon) => {
-      return {
-        pokemonName: newPokemon.Name,
-        pokemonType1: newPokemon.Type1,
-        pokemonType2: newPokemon.Type2,
-      };
-    });
-    // let currentData = JSON.parse(fs.readFileSync("db.json"));
-    // currentData.pokemons = newData;
-    // fs.writeFileSync("db.json", JSON.stringify(currentData));
+    let currentData = JSON.parse(fs.readFileSync("db.json"));
     let results = [];
-    results = newData;
+    results = currentData.pokemons;
 
     results = results.slice(offset, offset + limit);
 
@@ -32,6 +54,8 @@ const getPokemons = async (req, res, next) => {
     next(error);
   }
 };
+
+createPokemonData();
 
 router.get("/", getPokemons);
 

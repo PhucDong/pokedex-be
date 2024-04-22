6cf641c44def0f174ac360a3f3cfc7f6d80ce71d
Phuc <dongvuminhphuc@gmail.com>
2024-04-22 16:01:50 +0700

    Remov comments, add queries & their conditions

--- routes/pokemons.api.js
+++ routes/pokemons.api.js
@@ -4,35 +4,28 @@ const fs = require("fs");
 const path = require("path");
 const jsonFilePath = path.join(process.cwd(), "db.json");
 
-// const getPokemons = (req, res, next) => {
-//   try {
-//     let { page, limit, ...filterQuery } = req.query;
-//     page = parseInt(page) || 1;
-//     limit = parseInt(limit) || 10;
-//     let offset = limit * (page - 1);
-
-//     let currentData = JSON.parse(fs.readFileSync(jsonFilePath));
-//     let results = [];
-//     results = currentData.pokemons;
-
-//     results = results.slice(offset, offset + limit);
-
-//     res.status(200).send(results);
-//   } catch (error) {
-//     next(error);
-//   }
-// };
-
 router.get("/", (req, res, next) => {
   try {
-    let { page, limit } = req.query;
+    let { page, limit, search, type } = req.query;
     page = parseInt(page) || 1;
     limit = parseInt(limit) || 10;
     let offset = limit * (page - 1);
 
     let currentData = JSON.parse(fs.readFileSync(jsonFilePath, "utf-8"));
     let results = [];
-    results = currentData.pokemons;
+    results = currentData.pokemons; 
+
+    if (search) {
+      if (!isNaN(parseInt(search))) {
+        results = results.filter((pokemon) => pokemon.id === parseInt(search));
+      } else {
+        results = results.filter((pokemon) => pokemon.name.includes(search));
+      }
+    }
+
+    if (type) {
+      results = results.filter((pokemon) => pokemon.types.includes(type));
+    }
 
     results = results.slice(offset, offset + limit);
 
